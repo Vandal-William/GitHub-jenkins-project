9064182cd76f46d1688973d26b77d017345fcf56
Vandal-William <vandal.william@hotmail.com>
2023-03-10 18:18:13 +0100

    change methode to connect #6

--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const myPassword = process.env.ANSIBLE_SSH_PASS;
 
 const PORT = process.env.PORT || 4100;
 const app = express();
